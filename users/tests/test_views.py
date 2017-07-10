fa93d23c90ae76f833b32850f2e1f0606ad36040
John Griebel <jgriebel@3blades.io>
2017-07-10 12:45:59 -0400

    Allow users to view all of their own emails, as well as other users public emails.
    Signed-off-by: John Griebel <jgriebel@3blades.io>

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from users.tests.factories import UserFactory
+from users.tests.factories import UserFactory, EmailFactory
 
 
 class UserTest(APITestCase):
@@ -23,3 +23,43 @@ class UserTest(APITestCase):
         url = reverse('user-detail', kwargs={'pk': str(user.pk)})
         response = self.user_client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class EmailTest(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client = self.client_class(HTTP_AUTHORIZATION='Token {}'.format(self.user.auth_token.key))
+
+    def test_list_has_access_to_all_my_emails(self):
+        EmailFactory(public=True, user=self.user)
+        EmailFactory(public=False, user=self.user)
+
+        url = reverse("email-list", kwargs={'user_id': self.user.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_list_emails_gets_correct_ones(self):
+        other_email_public = EmailFactory(public=True)
+        other_email_private = EmailFactory(public=False,
+                                           user=other_email_public.user)
+
+        url = reverse("email-list", kwargs={'user_id': other_email_private.user.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+        resp_email = response.data[0]
+        self.assertEqual(resp_email['address'], other_email_public.address)
+        self.assertTrue(resp_email['public'])
+
+    def test_retrieve_doesnt_get_private_emails(self):
+        other_email_private = EmailFactory(public=False,)
+
+        url = reverse("email-detail", kwargs={'user_id': other_email_private.user.pk,
+                                              'pk': other_email_private.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
