6867dd7a60527e9ce2d0f55e896facfe89b09758
John Griebel <jgriebel@3blades.io>
2017-07-12 12:48:55 -0400

    Fix URL regex, remove patch version.
    Signed-off-by: John Griebel <jgriebel@3blades.io>

--- appdj/urls/base.py
+++ appdj/urls/base.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.conf import settings
 urlpatterns = [
-    url(r'^(?P<version>{major_version}\.?[0-9]*\.?[0-9]*)/'.format(major_version=settings.DEFAULT_VERSION),
+    url(r'^(?P<version>{major_version}(\.[0-9]+)?)/'.format(major_version=settings.DEFAULT_VERSION),
         include("appdj.urls.unversioned"))
 ]
