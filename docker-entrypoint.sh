ad4947967c5f5feef66c8e36c795b10941c7b1be
Michael D Roach <roach.d.michael@gmail.com>
2017-07-19 21:11:29 -0400

    Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 
 sleep 1
 /srv/env/bin/python /srv/app/manage.py migrate
-/srv/env/bin/python /srv/app/manage.py create_admin
+# /srv/env/bin/python /srv/app/manage.py create_admin
 /srv/env/bin/python /srv/app/manage.py create_resource
 /srv/env/bin/python /srv/app/manage.py site_host
+/srv/app/watchman_trigger.sh
 
 exec "$@"
