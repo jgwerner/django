a3d15f2e49d5d2f63818e517674c325189e11786
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-07-28 16:46:43 +0200

    Increase limit for action list view test
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- actions/tests/test_views.py
+++ actions/tests/test_views.py
@@ -39,7 +39,7 @@ class ActionTest(APITestCase):
         ActionFactory(content_object=EnvironmentResourcesFactory())
         ActionFactory(content_object=TriggerFactory())
         url = reverse('action-list', kwargs={'version': settings.DEFAULT_VERSION})
-        response = self.client.get(url)
+        response = self.client.get(url, {'limit': 100})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_action_details(self):
