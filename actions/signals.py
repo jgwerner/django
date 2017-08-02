e826dbd27642630981337c2e8cd99ca4692e2476
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-08-02 17:20:47 +0200

    Build url for trigger
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- actions/signals.py
+++ actions/signals.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.sites.models import Site
 
 from triggers.models import Trigger
 from .models import Action
@@ -9,5 +11,8 @@ from .models import Action
 def trigger_action(sender, instance, created, **kwargs):
     if created:
         return
+    current_site = Site.objects.get_current().domain
+    scheme = 'https' if settings.HTTPS else 'http'
+    url = f'{scheme}://{current_site}'
     for trigger in Trigger.objects.filter(cause=instance):
-        trigger.dispatch()
+        trigger.dispatch(url)
