b3158711bc54ff1671cb00f37f9125fedc0bf65d
John Griebel <jgriebel@3blades.io>
2017-08-02 11:21:13 -0400

    Generate random images without numpy. The images are only a solid color now, but that's fine.
    Signed-off-by: John Griebel <jgriebel@3blades.io>

--- users/tests/utils.py
+++ users/tests/utils.py
@@ -1,9 +1,11 @@
-import numpy
+from random import randint
 from PIL import Image
 
 
-def generate_random_image(name):
+def generate_random_image(name, height=100, width=100):
     # Thanks Stack OverFlow: https://stackoverflow.com/a/15262028/5627654
-    image_array = numpy.random.rand(100, 100, 3) * 255
-    image = Image.fromarray(image_array.astype('uint8')).convert("RGBA")
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+    image = Image.new(mode="RGB", size=(height, width), color=(red, green, blue))
     image.save("/tmp/" + name)
