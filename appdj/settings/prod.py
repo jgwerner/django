f487d4f3c252a51c41bb91c73bc40a940a9bbe7d
Jan Kowalski <jankowalskipraca85@gmail.com>
2017-08-02 17:29:55 +0200

    Remove unnecessary things from production settings
    Signed-off-by: Jan Kowalski <jankowalskipraca85@gmail.com>

--- appdj/settings/prod.py
+++ appdj/settings/prod.py
@@ -1,10 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['go-pilot.3blades.io', 'localhost']
+ALLOWED_HOSTS = ['api-dev.3blades.ai', 'localhost']
 if 'TBS_HOST' in os.environ:
     ALLOWED_HOSTS.append(os.environ['TBS_HOST'])
-
-LOGIN_REDIRECT_URL = '/swagger/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
