82289914edfe794f0595c57874c334a2124d494e
Michael D Roach <roach.d.michael@gmail.com>
2017-08-03 10:57:15 -0400

    Updated to actually Run the job
    Previous command just listed the jobs. Did that to make sure I was not going to break anything.
    
    This one will actually run the dev-deploy on master merge.

--- config/buildDockerImage.sh
+++ config/buildDockerImage.sh
@@ -8,5 +8,5 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
    docker push $DOCKER_IMAGE_NAME:build-$TRAVIS_BUILD_NUMBER;
    docker push $DOCKER_IMAGE_NAME:latest;
    # Auto Deploy To Rundeck
-   curl https://maestro-staging.3blades.io/api/14/project/3blades_legacy/jobs?authtoken=$RUNDECK_NONPROD_API_KEY
+   curl https://maestro-staging.3blades.io/3b97a4d4-fdd2-40cb-bf3f-a24de0e23ccc/run?authtoken=$RUNDECK_NONPROD_API_KEY
 fi
