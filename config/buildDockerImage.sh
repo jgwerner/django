4391f9211800e2bceef73dbe3ec71b5c0f848d76
Michael D Roach <roach.d.michael@gmail.com>
2017-09-07 10:41:10 -0400

    CHORE: Update build process

--- config/buildDockerImage.sh
+++ config/buildDockerImage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
    docker build --no-cache -t $DOCKER_IMAGE_NAME .;
    docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:build-$TRAVIS_BUILD_NUMBER;
@@ -8,7 +9,15 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
    docker push $DOCKER_IMAGE_NAME:build-$TRAVIS_BUILD_NUMBER;
    docker push $DOCKER_IMAGE_NAME:latest;
    # Auto Deploy To Rundeck dev-api.3blades.ai
-   echo "-- Starting Rundeck dev-api deploy"
-   curl https://maestro-staging.3blades.io/api/1/job/3b97a4d4-fdd2-40cb-bf3f-a24de0e23ccc/run?authtoken=$RUNDECK_KEY
-   echo "-- Done."
+   echo "-- Starting Autodeploy dev-api deploy"
+    curl https://maestro-staging.3blades.io/api/1/job/3b97a4d4-fdd2-40cb-bf3f-a24de0e23ccc/run?authtoken=$RUNDECK_KEY
+   echo "-- Done."a
+  fi
+  if [ "$TRAVIS_BRANCH" == "prod" ]; then
+   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+   docker build --no-cache -t $DOCKER_IMAGE_NAME .;
+   docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:prod-build-$TRAVIS_BUILD_NUMBER;
+   docker push $DOCKER_IMAGE_NAME:prod-build-$TRAVIS_BUILD_NUMBER;
+   echo "-- Done."a
+  fi
 fi
