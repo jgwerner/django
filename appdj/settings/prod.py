35b4aea8308771b028f7d9b8198aea7d8b99dbd9
John Griebel <jgriebel@3blades.io>
2017-09-27 07:04:11 -0400

    Add Sentry config for production.
    Signed-off-by: John Griebel <jgriebel@3blades.io>

--- appdj/settings/prod.py
+++ appdj/settings/prod.py
@@ -1,5 +1,13 @@
+import raven
 from .base import *
 
 ALLOWED_HOSTS = ['api-dev.3blades.ai', 'localhost']
 if 'TBS_HOST' in os.environ:
     ALLOWED_HOSTS.append(os.environ['TBS_HOST'])
+
+INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+
+RAVEN_CONFIG = {
+    'dsn': f'https://{os.getenv("SENTRY_DSN")}@sentry.io/85124',
+    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+}
