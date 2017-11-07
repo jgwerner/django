e683ed8aed765f53cd74f8678bf34c1faff0c090
John Griebel <jgriebel@3blades.io>
2017-11-07 11:19:11 -0500

    Pass request as part of context for user serializer
    Signed-off-by: John Griebel <jgriebel@3blades.io>

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -506,6 +506,36 @@ class UserTest(APITestCase):
         self.assertEqual(response.data['id'], str(self.admin.id))
         self.assertEqual(response.data['username'], self.admin.username)
 
+    def test_me_endpoint_serializes_avatar_correctly(self):
+        # Upload the file and at least ensure a 200 was returned
+        url = reverse("avatar", kwargs={'version': settings.DEFAULT_VERSION,
+                                        'user_pk': self.user.pk})
+
+        generate_random_image("myavatar.png")
+        image_upload = SimpleUploadedFile(name="myavatar.png",
+                                          content=open("/tmp/myavatar.png", "rb").read(),
+                                          content_type="image/png")
+
+        data = {'image': image_upload}
+        response = self.user_client.post(url, data=data, format="multipart")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        url = reverse("me", kwargs={'version': settings.DEFAULT_VERSION})
+        response = self.user_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], str(self.user.id))
+
+        user_reloaded = User.objects.get(pk=self.user.pk)
+        request = response.wsgi_request
+        full_url = ("https" + "://" +
+                    request.environ['SERVER_NAME'] +
+                    user_reloaded.profile.avatar.url)
+
+        self.assertEqual(response.data['profile']['avatar'], full_url)
+
+
+
 
 class EmailTest(APITestCase):
     def setUp(self):
