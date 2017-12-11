224a8965997a4c977d0bfd406d807c63dbd833f2
Josh Starrett <jstarrett@3blades.io>
2017-12-11 10:37:20 -0500

    increase wait for api timeout

--- api_tests/wait_for_api.sh
+++ api_tests/wait_for_api.sh
@@ -6,7 +6,7 @@ set -e
 cmd="$@"
 url="$HOST:$PORT/tbs-status"
 echo "Attempting to reach $url"
-max_attempts=30
+max_attempts=60
 attempt=0
 until $(curl --output /dev/null --silent --head --fail $url) || [ $attempt -eq $max_attempts ]; do
     sleep 1
