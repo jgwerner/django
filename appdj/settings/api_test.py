9cce357af5cdad2e9baaea521c36c063ed03c4e5
Josh Starrett <jstarrett@3blades.io>
2017-12-11 09:40:57 -0500

    Add settings file for api tests

---
+++ appdj/settings/api_test.py
@@ -0,0 +1,28 @@
+import raven
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+    'rest_framework.renderers.BrowsableAPIRenderer',
+)
+
+REST_FRAMEWORK['DEFAULT_PARSER_CLASSES'] = (
+    'rest_framework.parsers.JSONParser',
+    'rest_framework.parsers.FormParser',
+    'rest_framework.parsers.MultiPartParser'
+)
+
+INSTALLED_APPS.extend(['debug_toolbar', 'rest_framework_swagger',
+                       'swagger_docs', 'raven.contrib.django.raven_compat'])
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+INTERNAL_IPS = ['127.0.0.1']
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
