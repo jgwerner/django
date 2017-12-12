74b98fd2182e579e92564814cf774c27e59f329c
Josh Starrett <jstarrett@3blades.io>
2017-12-12 13:14:25 -0500

    Debugging unit tests in travis

--- travis/script.sh
+++ travis/script.sh
@@ -1,7 +1,6 @@
 if [ "$TEST_SUITE" = "UNIT" ]
 then
-    python -W ignore tbs_coverage.py run manage.py test --parallel 16 
-    coverage combine && coverage report -m
+    python -W ignore tbs_coverage.py run manage.py test --parallel 16 && coverage combine && coverage report -m
 elif [ "$TEST_SUITE" = "API" ]
 then
     docker-compose -f docker-compose-test.yml up --abort-on-container-exit
