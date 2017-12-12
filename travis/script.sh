b0e566ffa565c473a2478540ce9cf3bb783b72a3
Josh Starrett <jstarrett@3blades.io>
2017-12-12 08:15:14 -0500

    fix script.sh

--- travis/script.sh
+++ travis/script.sh
@@ -3,5 +3,6 @@ then
     python -W ignore tbs_coverage.py run manage.py test --parallel 16 
     coverage combine && coverage report -m
 elif [ "$TEST_SUITE" = "API" ]
+then
     docker-compose -f docker-compose.test.yml up --abort-on-container-exit
 fi
