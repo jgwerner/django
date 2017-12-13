9efdd55be1928a7b3b1395241d700ae4b3c34c49
Josh Starrett <jstarrett@3blades.io>
2017-12-13 08:01:46 -0500

    Add some more tests and a readme

--- api_tests/me_test.js
+++ api_tests/me_test.js
@@ -36,7 +36,7 @@ describe('me', () => {
         }
     }
 
-    it('GET me should return me', () => {
+    it('GET should return me', () => {
         let uri = tools.get_request_uri("me");
         let response = chakram.get(uri, this.options);
         expect(response).to.have.status(200);
@@ -46,19 +46,34 @@ describe('me', () => {
         expect(response).to.have.header('content-type', 'application/json');
         return chakram.wait();
     });
+});
 
-    it('POST me/teams should create a new team', () => {
+describe('me/teams', () => {
+
+    this.team;
+    let team_json = {
+        name: "TestTeam",
+        description: "TestTeam",
+        website: "http://mywebsite.com",
+        location: "here"
+    };
+    afterEach(() => {
         let uri = tools.get_request_uri("me/teams/");
-        let team_json = {
-            name: "TestTeam",
-            description: "TestTeam",
-            website: "http://mywebsite.com",
-            location: "here"
-        };
+        if (this.team) {
+            let response = chakram.delete(uri + this.team + "/", {}, this.options)
+            this.team = undefined;
+            expect(response).to.have.status(204);
+            return chakram.wait();
+        }
+    });
+
+    it('POST a valid team should create a new team', () => {
+        let uri = tools.get_request_uri("me/teams/");
         return chakram.post(uri, team_json, this.options)
             .then(response => {
                 expect(response).to.have.status(201);
                 expect(response).to.comprise.of.json(team_json);
+                this.team = response.body.name
                 uri += team_json.name
                 return chakram.get(uri, this.options);
             })
@@ -67,4 +82,58 @@ describe('me', () => {
                 expect(response).to.comprise.of.json(team_json);
             });
     });
-})
+
+    it('DELETE an existing team should remove the team', () => {
+        let uri = tools.get_request_uri("me/teams/");
+        return chakram.post(uri, team_json, this.options)
+            .then(response => {
+                expect(response).to.have.status(201);
+                expect(response).to.comprise.of.json(team_json);
+                uri += team_json.name + "/";
+                return chakram.delete(uri, {}, this.options);
+            })
+            .then(response => {
+                expect(response).to.have.status(204);
+                return chakram.get(uri, this.options);
+            })
+            .then(response => {
+                expect(response).to.have.status(404);
+            });
+    });
+
+    it('PATCH an existing team should modify a team', () => {
+        let uri = tools.get_request_uri("me/teams/");
+        let modified_team = JSON.parse(JSON.stringify(team_json));
+        return chakram.post(uri, team_json, this.options)
+            .then(response => {
+                expect(response).to.have.status(201);
+                expect(response).to.comprise.of.json(team_json);
+                uri += team_json.name + "/";
+                modified_team.name = "TestTeam123"
+                return chakram.patch(uri, modified_team, this.options);
+            })
+            .then(response => {
+                expect(response).to.have.status(200);
+                this.team = modified_team.name;
+                expect(response).to.comprise.of.json(modified_team);
+            });
+    });
+
+    it('PUT an existing team should modify a team', () => {
+        let uri = tools.get_request_uri("me/teams/");
+        let modified_team = JSON.parse(JSON.stringify(team_json));
+        return chakram.post(uri, team_json, this.options)
+            .then(response => {
+                expect(response).to.have.status(201);
+                expect(response).to.comprise.of.json(team_json);
+                uri += team_json.name + "/";
+                modified_team.name = "TestTeam123"
+                return chakram.put(uri, modified_team, this.options);
+            })
+            .then(response => {
+                expect(response).to.have.status(200);
+                this.team = modified_team.name;
+                expect(response).to.comprise.of.json(modified_team);
+            });
+    });
+});
