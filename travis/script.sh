dfc50b7896743897a8b43e83688892bc8fe8bed6
Josh Starrett <jstarrett@3blades.io>
2017-12-21 14:29:00 -0500

    Try skipping project_files tests

--- travis/script.sh
+++ travis/script.sh
@@ -5,7 +5,6 @@ then
     python -W ignore tbs_coverage.py run manage.py test --parallel 16 && coverage combine && coverage report -m
 elif [ "$TEST_SUITE" = "API" ]
 then
-    docker-compose -f docker-compose-test.yml build --no-cache
     docker-compose -f docker-compose-test.yml up --abort-on-container-exit
     docker logs appbackend_test_1
 fi
