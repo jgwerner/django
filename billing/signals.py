bd31b1745069bce79ec4539c7d60c41c8f9d60d5
John Griebel <johnkgriebel@gmail.com>
2018-01-10 08:14:20 -0500

    Don't check subscription status for AnonymousUser
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- billing/signals.py
+++ billing/signals.py
@@ -1,22 +1,23 @@
 import logging
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from billing.models import Customer, Plan
+from users.models import User
+from billing.models import Customer
 from billing.stripe_utils import (create_stripe_customer_from_user,
                                   assign_customer_to_default_plan)
 log = logging.getLogger("billing")
 
 
-@receiver(post_save, sender=get_user_model())
+@receiver(post_save, sender=User)
 def check_if_customer_exists_for_user(sender, instance, created, **kwargs):
-    user = instance
-    if settings.ENABLE_BILLING:
-        try:
-            user.customer
-        except Customer.DoesNotExist:
-            log.info("No stripe customer exists for user {uname}. "
-                     "Creating one.".format(uname=user.username))
-            customer = create_stripe_customer_from_user(user)
-            assign_customer_to_default_plan(customer)
+    if sender == User:
+        user = instance
+        if settings.ENABLE_BILLING and user.username != "AnonymousUser":
+            try:
+                user.customer
+            except Customer.DoesNotExist:
+                log.info("No stripe customer exists for user {uname}. "
+                         "Creating one.".format(uname=user.username))
+                customer = create_stripe_customer_from_user(user)
+                assign_customer_to_default_plan(customer)
