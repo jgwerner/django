37ec7a1350587d548ea5137a99260b2e16cf6ed6
John Griebel <johnkgriebel@gmail.com>
2018-01-10 10:05:50 -0500

    loaddata earlier in docker entrypoint script.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 sleep 1
 
 /srv/env/bin/python /srv/app/manage.py migrate
+/srv/env/bin/python /srv/app/manage.py loaddata plans.json
 /srv/env/bin/python /srv/app/manage.py create_admin
 /srv/env/bin/python /srv/app/manage.py create_server_size
 /srv/env/bin/python /srv/app/manage.py site_host
-/srv/env/bin/python /srv/app/manage.py loaddata plans.json
 /srv/app/watchman_trigger.sh
 
 exec "$@"
