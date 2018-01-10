ccef2d75ad47524b6d37c0b3d48ae05baa693b51
John Griebel <johnkgriebel@gmail.com>
2018-01-10 07:29:49 -0500

    Move loaddata call to correct place
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ sleep 1
 /srv/env/bin/python /srv/app/manage.py create_admin
 /srv/env/bin/python /srv/app/manage.py create_server_size
 /srv/env/bin/python /srv/app/manage.py site_host
+/srv/env/bin/python /srv/app/manage.py loaddata plans.json
 /srv/app/watchman_trigger.sh
 
 exec "$@"
