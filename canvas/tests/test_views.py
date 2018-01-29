66236f067debd16a4e2d635d6b5e6b782b8301cd
Jan Kowalski <jankowalskipraca85@gmail.com>
2018-01-29 16:20:26 +0100

    Tests

--- canvas/tests/test_views.py
+++ canvas/tests/test_views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -5,6 +7,8 @@ from rest_framework.test import APITestCase
 
 class CanvasViewsTestCase(APITestCase):
     def test_canvas_xml(self):
-        url = reverse('canvas-xml')
+        url = reverse('canvas-xml', kwargs={'version': settings.DEFAULT_VERSION})
         resp = self.client.get(url)
+        site = Site.objects.get_current()
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertIn(f'{site.domain}{url}', resp.data[2]['value'])
