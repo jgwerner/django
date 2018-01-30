e96930523f8c572b03c3f3cb16ddb939ebc4f7b8
Jan Kowalski <jankowalskipraca85@gmail.com>
2018-01-30 12:55:56 +0100

    Add application id

--- canvas/serializers.py
+++ canvas/serializers.py
@@ -7,6 +7,6 @@ class ApplicationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Application
-        fields = ('client_id', 'redirect_uris', 'client_type', 'client_secret',
+        fields = ('id', 'client_id', 'redirect_uris', 'client_type', 'client_secret',
                   'name', 'authorization_grant_type', 'user')
         read_only_fields = ('client_id', 'client_secret')
