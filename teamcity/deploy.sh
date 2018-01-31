d3638a50f8a3bc76dac0c6c8f8031c1ee0c191cc
Josh Starrett <jstarrett@3blades.io>
2018-01-31 08:55:17 -0500

    Add teamcity test runner to django unit tests

--- teamcity/deploy.sh
+++ teamcity/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-   docker build --no-cache -t $DOCKER_IMAGE_NAME .;
-   docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:build-$DOCKER_IMG_NUMBER;
-   docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest;
-   docker push $DOCKER_IMAGE_NAME:build-$DOCKER_IMG_NUMBER;
-   docker push $DOCKER_IMAGE_NAME:latest;
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker build --no-cache -t $DOCKER_IMAGE_NAME .;
+docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:build-$DOCKER_IMG_NUMBER;
+docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest;
+docker push $DOCKER_IMAGE_NAME:build-$DOCKER_IMG_NUMBER;
+docker push $DOCKER_IMAGE_NAME:latest;
--- teamcity/run_unit_tests.sh
+++ teamcity/run_unit_tests.sh
@@ -18,5 +18,8 @@ docker ps
 python3.6 -m pip install -U pip setuptools wheel
 python3.6 -m pip install -r requirements/dev.txt
 python3.6 -m pip install codecov
+python3.6 -m pip install teamcity-messages
+
+echo 'TEST_RUNNER = "teamcity.django.TeamcityDjangoRunner"' >> appdj/settings/test.py
 
 python3.6 -W ignore tbs_coverage.py run manage.py test --parallel 16 && coverage combine && coverage report -m
