e59319149ed46261b97e49ab70184a4b7d4ca445
Josh Starrett <jstarrett@3blades.io>
2018-01-31 08:52:55 -0500

    Add initial scripts
    Pushing to server so I can test in teamcity

---
+++ teamcity/run_api_tests.sh
@@ -0,0 +1,9 @@
+set -e
+
+
+finish(){
+    docker logs appbackend_test_1
+}
+trap finish EXIT
+
+docker-compose -f docker-compose-test.yml -p appbackend up --build --abort-on-container-exit
---
+++ teamcity/run_unit_tests.sh
@@ -0,0 +1,22 @@
+cleanup(){
+    docker stop test-postgres
+    docker rm test-postgres
+    docker stop test-redis
+    docker rm test-redis
+    
+    rm -rf /workspaces/
+    rm -rf /tmp/3blades/
+}
+
+trap cleanup EXIT
+
+docker run --name test-postgres -p 5432:5432 -d postgres
+docker run --name test-redis -p 6379:6379 -d redis
+
+docker ps
+
+python3.6 -m pip install -U pip setuptools wheel
+python3.6 -m pip install -r requirements/dev.txt
+python3.6 -m pip install codecov
+
+python3.6 -W ignore tbs_coverage.py run manage.py test --parallel 16 && coverage combine && coverage report -m
