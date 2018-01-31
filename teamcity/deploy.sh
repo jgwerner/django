9267027e9c268cb71d62dd5ba36a2f08a2fce241
Josh Starrett <jstarrett@3blades.io>
2018-01-31 10:45:38 -0500

    Add documentation to scripts

--- teamcity/deploy.sh
+++ teamcity/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+###
+###
+###
+###   Purpose:      Tag and push app-backend as build number and latest. Authentication is handled in teamcity.
+###   Author:       Josh Starrett (jstarrett@3blades.io)
+###
+### ######################################### ###
 
 docker build --no-cache -t $DOCKER_IMAGE_NAME .;
 docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:build-$DOCKER_IMG_NUMBER;
--- teamcity/run_unit_tests.sh
+++ teamcity/run_unit_tests.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+###
+###
+###
+###   Purpose:      Run unit tests inside teamcity build.
+###   Author:       Josh Starrett (jstarrett@3blades.io)
+###
+### ######################################### ###
+
+# Cleanup docker containers and cleanup directories
 cleanup(){
     docker stop test-postgres
     docker rm test-postgres
@@ -7,19 +17,17 @@ cleanup(){
     rm -rf /workspaces/
     rm -rf /tmp/3blades/
 }
-
 trap cleanup EXIT
-
+# Startup test dependencies in containers
 docker run --name test-postgres -p 5432:5432 -d postgres
 docker run --name test-redis -p 6379:6379 -d redis
-
 docker ps
-
+# Install python package dependencies
 python3.6 -m pip install -U pip setuptools wheel
 python3.6 -m pip install -r requirements/dev.txt
 python3.6 -m pip install codecov
 python3.6 -m pip install teamcity-messages
-
+# Setup teamcity test runner so we can test results in teamcity
 echo 'TEST_RUNNER = "teamcity.django.TeamcityDjangoRunner"' >> appdj/settings/test.py
-
+# Run the tests
 python3.6 -W ignore tbs_coverage.py run manage.py test --parallel 16 && coverage combine && coverage report -m
