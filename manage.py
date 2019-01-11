e1086af854b7d0152c192d7b07d8b30177834a42
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2019-01-11 13:09:55 -0600

    Use django-environ to read env-vars from .env file and with this simplify local setup

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
+    environ.Env.read_env() # reading .env file
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appdj.settings")
     try:
         from django.core.management import execute_from_command_line
