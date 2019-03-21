d2b71c1972b4762079f4a2d43c45b9c9386b6aa7
jkp85 <jkp85@users.noreply.github.com>
2019-03-21 11:45:41 +0100

    fix: oauth application create fix (#175)

--- appdj/oauth2/serializers.py
+++ appdj/oauth2/serializers.py
@@ -8,10 +8,10 @@ ProviderApplication = get_application_model()
 
 class ApplicationSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(source='application.user', default=serializers.CurrentUserDefault())
-    client_id = serializers.CharField(source='application.client_id')
+    client_id = serializers.CharField(source='application.client_id', required=False)
     redirect_uris = serializers.CharField(source='application.redirect_uris')
     client_type = serializers.CharField(source='application.client_type')
-    client_secret = serializers.CharField(source='application.client_secret')
+    client_secret = serializers.CharField(source='application.client_secret', required=False)
     name = serializers.CharField(source='application.name')
     authorization_grant_type = serializers.CharField(source='application.authorization_grant_type')
 
@@ -19,4 +19,7 @@ class ApplicationSerializer(serializers.ModelSerializer):
         model = Application
         fields = ('id', 'client_id', 'redirect_uris', 'client_type', 'client_secret',
                   'name', 'authorization_grant_type', 'user')
-        read_only_fields = ('client_id', 'client_secret')
+
+    def create(self, validated_data):
+        pr_app = ProviderApplication.objects.create(**validated_data['application'])
+        return Application.objects.create(application=pr_app)
