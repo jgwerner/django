95b356b9438350785b5acf7e6643dc338fd15c9f
Greg Werner <werner.greg@gmail.com>
2019-09-05 10:00:27 -0400

    chore: Remove aws cdk (#452)
    - removed unused aws cdk scripts

--- scripts/IllumiDeskStack/bin/illumi_desk_stack.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/cdk');
-import { IllumiDeskStackStack } from '../lib/illumi_desk_stack-stack';
-
-const app = new cdk.App();
-
-new IllumiDeskStackStack(app, 'IllumiDeskStackStack');
